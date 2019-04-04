1d12931daf4b00ad6ce259effe31bc2afb18cb88
“Kavitha” <kavithakamal@gmail.com>
2019-04-04 11:37:20 -0400

    Added the skeleton code in slackbot.py

--- slackbot.py
+++ slackbot.py
@@ -1,7 +1,63 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A slackbot that responds to commands.
+This uses the Slack RTM (Real Time Messaging) API.
+Required environment variables (example only, these are not real tokens).
+Get these from the Slack account settings that you are connecting to.
+   BOT_USER_ID = 'U20981S736'
+   BOT_USER_TOKEN = 'xoxb-106076235608-AbacukynpGahsicJqugKZC'
+"""
+__author__ = '???'
+BOT_NAME = 'my-bot-name'
+BOT_CHAN = '#bot-test'
+
+
+def config_logger():
+    """Setup logging configuration"""
+
+
+def command_loop(bot):
+    """Process incoming bot commands"""
+    pass
+
+
+def signal_handler(sig_num, frame):
+    pass
+
+
+class SlackBot:
+
+    def __init__(self, bot_user_token, bot_id=None):
+        """Create a client instance"""
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __enter__(self):
+        """Implement this method to make this a context manager"""
+        pass
+
+    def __exit__(self, type, value, traceback):
+        """Implement this method to make this a context manager"""
+        pass
+
+    def post_message(self, msg, chan=BOT_CHAN):
+        """Sends a message to a Slack Channel"""
+        pass
+
+    def handle_command(self, twc, raw_cmd, channel):
+        """Parses a raw command string from the bot"""
+        pass
+
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
